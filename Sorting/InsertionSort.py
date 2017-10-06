d173445112b81f5a30ff13bccb8eba1a81106633
José Manuel Llorens <jose.llorens.ripolles@gmail.com>
2017-10-06 12:08:31 +0200

    Added comments to Insertion Sort

--- Sorting/InsertionSort.py
+++ Sorting/InsertionSort.py
@@ -3,7 +3,13 @@ import sys
 
 def insertion_sort(sequence):
 
+    # For each element, e1. We start with 1, this way the initial list is sorted.
     for i in range(1, len(sequence)):
+
+        # We search backwards a element e2 minor than e1, because the list from 0 to i is sorted, the fist minor element
+        # will be the greatest of them. Each time we go backwards if e2 is greater than e1 we copy it on the right
+        # adjacent position, this way when we find the minor element we just copy  e1 at e2 position +1
+
         for j in range(i, 0, -1):
             if sequence[j-1] > sequence[j]:
                 aux = sequence[j-1]
@@ -15,6 +21,6 @@ def insertion_sort(sequence):
     return sequence
 
 if __name__ == "__main__":
-    sequence = sys.argv[1:len(sys.argv)]
-    sequence = list(map(int, sequence))
-    print(insertion_sort(sequence))
+    sequence_input = sys.argv[1:len(sys.argv)]
+    sequence_parsed = list(map(int, sequence_input))
+    print(insertion_sort(sequence_parsed))
