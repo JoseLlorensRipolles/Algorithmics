f77c9d27bc2b16c677d7740975baf63aac70b95e
JoseLlorensRipolles <jose.llorens.ripolles@gmail.com>
2017-10-13 03:35:29 +0200

    Partially implemented Heap data structure

--- DataStructures/src/Heap.py
+++ DataStructures/src/Heap.py
@@ -1,7 +1,41 @@
 class Heap:
 
-    def __init__(self, sequence=[]):
-        self.heap = sequence
+    def __init__(self, array=[]):
+
+        self.array = []
+
+        for i in range(len(array)):
+            self.array.append(array[i])
+            self.array = Heap.sift_up(self.array, i)
+
+    @staticmethod
+    def sift_up(array, i):
+
+        if (len(array) is 0) or i >= len(array):
+            return array
+
+        while array[int((i-1)/2)] < array[i]:
+            father = int((i-1)/2)
+            aux = array[i]
+            array[i] = array[father]
+            array[father] = aux
+            i = father
+
+        return array
 
     def get_heap(self):
-        return self.heap
+        return self.array
+
+    def insert(self, new_value):
+        self.array.append(new_value)
+        self.array = Heap.sift_up(self.array, len(self.array)-1)
+
+    def find_max(self):
+
+        if self.array is []:
+            return None
+
+        return self.array[0]
+
+
+
--- DataStructures/test/HeapTest.py
+++ DataStructures/test/HeapTest.py
@@ -1,12 +1,53 @@
+import random
 import unittest
-import DataStructures.src.Heap as Heap
+from DataStructures.src.Heap import Heap
 
 
 class HeapTest(unittest.TestCase):
 
     def test_getter(self):
+
         heap = Heap()
-        self.assertEqual(heap.get_heap(), "")
+        self.assertEqual(heap.get_heap(), [])
+
+    def test_sift_up(self):
+
+        values = [10, 5, 7, 12, 3, 2, 4]
+        correct_heap = [12, 10, 7, 5, 3, 2, 4]
+        self.assertEqual(Heap.sift_up(values, 3), correct_heap)
+
+        values = [10, 5, 7, 8, 3, 2, 4]
+        correct_heap = [10, 8, 7, 5, 3, 2, 4]
+        self.assertEqual(Heap.sift_up(values, 3), correct_heap)
+
+        self.assertEqual(Heap.sift_up([], 1), [])
+
+        values = [10, 8, 7, 5, 3, 2, 4]
+        self.assertEqual(Heap.sift_up(values, 7), values)
+
+    def test_init(self):
+
+        heap = Heap([7, 2, 43, 52, 24, 32, 35, 14, 31, 57, 79, 3, 66, 6, 45])
+        correct_heap_array = [79, 57, 66, 31, 52, 35, 45, 2, 14, 24, 43, 3, 7, 6, 32]
+        self.assertEqual(heap.get_heap(), correct_heap_array)
+
+    def test_insert(self):
+
+        heap = Heap([7, 2, 43, 52, 24, 32, 35, 14, 31, 57, 79, 3, 66, 6, 45])
+        heap.insert(78)
+        correct_heap_array = [79, 78, 66, 57, 52, 35, 45, 31, 14, 24, 43, 3, 7, 6, 32, 2]
+        self.assertEqual(heap.get_heap(), correct_heap_array)
+
+        heap = Heap([])
+        heap.insert(90)
+        correct_heap_array = [90]
+        self.assertEqual(heap.get_heap(),correct_heap_array)
+
+    def test_find_max(self):
+
+        values = [random.randint(0, 1000) for r in range(10000)]
+        heap = Heap(values)
+        self.assertEqual(heap.find_max(), max(values))
 
 
 if __name__ == '__main__':
